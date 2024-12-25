b216c3b40c5362fc9a8c400d390cbe2d76442643
Fevzi Babaoğlu <fevzibabaoglu03@gmail.com>
2024-12-26 00:02:01 +0300

    feat: implement udp flood to discover peers

---
+++ src/main/java/com/github/fevzibabaoglu/BroadcastManager.java
@@ -0,0 +1,158 @@
+package com.github.fevzibabaoglu;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
+public class BroadcastManager {
+
+    private static final int BROADCAST_PORT = 8000;
+    private static final int RESPONSE_PORT = 8001;
+    private static final int BUFFER_SIZE_BYTES = 1024 * 1024;
+    private static final int TTL = 3;
+
+    private Peer localPeer;
+    private Peer tempLocalPeer;
+
+    public BroadcastManager() throws SocketException {
+        localPeer = new Peer();
+        tempLocalPeer = new Peer();
+    }
+
+    public void clearPeerCache() throws SocketException {
+        localPeer = new Peer();
+        tempLocalPeer = new Peer();
+    }
+
+    public Peer getLocalPeer() {
+        return localPeer;
+    }
+
+    // Broadcast a discovery message on all network interfaces
+    public void sendBroadcasts(DiscoveryMessage message) throws IOException {
+        if (message == null) {
+            message = new DiscoveryMessage(TTL, tempLocalPeer);
+        }
+
+        try (DatagramSocket socket = new DatagramSocket()) {
+            socket.setBroadcast(true);
+
+            for (PeerNetworkInterface localPeerNetworkInterface : tempLocalPeer.getPeerNetworkInterfaces()) {
+                // Exclude the interface where the message came from
+                if (localPeerNetworkInterface.equals(message.getInInterfaceByIndex(-1))) {
+                    continue;
+                }
+                
+                InetAddress broadcastIPAddress = localPeerNetworkInterface.getBroadcastIPAddress();
+                InetAddress localIPAddress = localPeerNetworkInterface.getLocalIPAddress();
+
+                // Copy the message, do not edit same message more than once
+                DiscoveryMessage copyMessage = message.clone();
+                
+                // Add the out interface to route list
+                copyMessage.addToInterfaceList(localPeerNetworkInterface);
+
+                System.out.printf("[%s] %s broadcasted on %s\n", localIPAddress, copyMessage.getOwner().getPeerNetworkInterfaces(), broadcastIPAddress);
+
+                // Send broadcast
+                byte[] messageByte = copyMessage.serialize();
+                DatagramPacket packet = new DatagramPacket(messageByte, messageByte.length, broadcastIPAddress, BROADCAST_PORT);
+                socket.send(packet);
+            }
+        }
+    }
+
+    // Listen for incoming broadcast messages on all network interfaces
+    public void listenBroadcasts() throws IOException, ClassNotFoundException {
+        try (DatagramSocket socket = new DatagramSocket(BROADCAST_PORT)) {
+            socket.setBroadcast(true);
+
+            byte[] buffer = new byte[BUFFER_SIZE_BYTES];
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+
+            while (true) {
+                socket.receive(packet);
+                DiscoveryMessage receivedMessage = DiscoveryMessage.deserialize(packet.getData(), packet.getLength());
+
+                InetAddress receiveIPAddress = packet.getAddress();
+                PeerNetworkInterface localPeerNetworkInterface = NetworkUtils.SubnetMatch(tempLocalPeer, receiveIPAddress);
+                InetAddress localIPAddress = localPeerNetworkInterface.getLocalIPAddress();
+
+                // If receiver is the sender
+                if (receiveIPAddress.getHostAddress().equals(localIPAddress.getHostAddress())){
+                    continue;
+                }
+
+                // Add the in interface to route list
+                receivedMessage.addToInterfaceList(localPeerNetworkInterface);
+
+                // Add the localPeer to previous peer's known peers (index -1 is localPeer, index -2 is previous peer)
+                Peer previousPeer = receivedMessage.getRoutePeerByIndex(-2);
+                previousPeer.addKnownPeerToInterface(receivedMessage.getOutInterfaceByIndex(-1), tempLocalPeer);
+
+                System.out.printf("[%s] Broadcast of %s is captured\n", localIPAddress, receivedMessage.getOwner().getPeerNetworkInterfaces());
+
+                // Forward broadcast
+                receivedMessage.decreaseTtl();
+                if (receivedMessage.getTtl() > 0) {
+                    sendBroadcasts(receivedMessage);
+                }
+
+                // Send response
+                sendResponse(receivedMessage);
+            }
+        }
+    }
+
+    // Send a response for incoming discovery broadcast
+    public void sendResponse(DiscoveryMessage message) throws IOException {
+        // Find previous message broadcaster
+        PeerNetworkInterface sendPeerNetworkInterface  = message.getOutInterfaceByIndex(-1);
+        InetAddress sendIPAddress = sendPeerNetworkInterface.getLocalIPAddress();
+
+        // Find local address
+        PeerNetworkInterface localPeerNetworkInterface = NetworkUtils.SubnetMatch(tempLocalPeer, sendIPAddress);
+        InetAddress localIPAddress = localPeerNetworkInterface.getLocalIPAddress();
+
+        // Remove last hop
+        message.removeLastHopFromInterfaceList();
+
+        System.out.printf("[%s] Response for %s sent/forwarded to %s\n", localIPAddress, message.getOwner().getPeerNetworkInterfaces(), sendIPAddress);
+
+        byte[] messageByte = message.serialize();
+        DatagramPacket responsePacket = new DatagramPacket(messageByte, messageByte.length, sendIPAddress, RESPONSE_PORT);
+        try (DatagramSocket responseSocket = new DatagramSocket()) {
+            responseSocket.send(responsePacket);
+        }
+    }
+
+
+    // Listen for incoming responses to the discovery broadcast
+    public void listenResponses() throws IOException, ClassNotFoundException {
+        try (DatagramSocket socket = new DatagramSocket(RESPONSE_PORT)) {
+
+            byte[] buffer = new byte[BUFFER_SIZE_BYTES];
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+
+            while (true) {
+                socket.receive(packet);
+                DiscoveryMessage receivedMessage = DiscoveryMessage.deserialize(packet.getData(), packet.getLength());
+
+                InetAddress receivedIPAddress = packet.getAddress();
+                PeerNetworkInterface localPeerNetworkInterface = NetworkUtils.SubnetMatch(tempLocalPeer, receivedIPAddress);
+                InetAddress localIPAddress = localPeerNetworkInterface.getLocalIPAddress();
+
+                // If receivedMessage owner is the localPeer, update the localPeer with new info, else forward response
+                if (NetworkUtils.IPMatch(receivedMessage.getOwner(), localIPAddress) != null) {
+                    localPeer.mergePeer(receivedMessage.getOwner());
+                    System.out.printf("[%s] Discovered new peers\n", localIPAddress);
+                } else {
+                    sendResponse(receivedMessage);
+                }
+
+            }
+        }
+    }
+}
---
+++ src/main/java/com/github/fevzibabaoglu/NetworkUtils.java
@@ -0,0 +1,54 @@
+package com.github.fevzibabaoglu;
+
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+public class NetworkUtils {
+
+    // Find the PeerNetworkInterface of the subnet that the localIP is in
+    public static PeerNetworkInterface IPMatch(Peer peer, InetAddress localIPAddress) {
+        for (PeerNetworkInterface localPeerNetworkInterface : peer.getPeerNetworkInterfaces()) {
+            // Check if the local IP is in
+            if (localPeerNetworkInterface.getLocalIPAddress().getHostAddress().equals(localIPAddress.getHostAddress())) {
+                return localPeerNetworkInterface;
+            }
+        }
+        return null;
+    }
+
+    // Find the PeerNetworkInterface of the subnet that the targetIp is in
+    public static PeerNetworkInterface SubnetMatch(Peer peer, InetAddress targetIPAddress) throws UnknownHostException, SocketException {
+        for (PeerNetworkInterface localPeerNetworkInterface : peer.getPeerNetworkInterfaces()) {
+            // Check if the target IP is within the same subnet
+            if (isInSameSubnet(localPeerNetworkInterface, targetIPAddress)) {
+                return localPeerNetworkInterface;
+            }
+        }
+        return null;
+    }
+
+    private static boolean isInSameSubnet(PeerNetworkInterface localPeerNetworkInterface, InetAddress targetIPAddress) {
+        InetAddress localIPAddress = localPeerNetworkInterface.getLocalIPAddress();
+
+        // Get the subnet mask
+        int prefixLength = localPeerNetworkInterface.getMaskLength();
+        int subnetMask = 0xFFFFFFFF << (32 - prefixLength);
+
+        // Convert IP addresses to integers
+        int targetIpInt = ipToInt(targetIPAddress.getHostAddress());
+        int localIpInt = ipToInt(localIPAddress.getHostAddress());
+
+        // Check if the target IP is within the same subnet
+        return ((targetIpInt & subnetMask) == (localIpInt & subnetMask));
+    }
+
+    private static int ipToInt(String ipAddress) {
+        String[] octets = ipAddress.split("\\.");
+        int result = 0;
+        for (int i = 0; i < 4; i++) {
+            result |= (Integer.parseInt(octets[i]) & 0xFF) << (24 - (i * 8));
+        }
+        return result;
+    }
+}
