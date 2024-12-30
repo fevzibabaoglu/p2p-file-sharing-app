c697e2e19cdc250bb0d2fc8d1e7a7a25ed8f9db4
Fevzi Babaoğlu <fevzibabaoglu03@gmail.com>
2024-12-31 00:11:05 +0300

    feat: implement file requesting

--- src/main/java/com/github/fevzibabaoglu/network/file_transfer/FileTransferManager.java
+++ src/main/java/com/github/fevzibabaoglu/network/file_transfer/FileTransferManager.java
@@ -8,6 +8,7 @@ import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
 import java.util.Set;
 
@@ -45,7 +46,7 @@ public class FileTransferManager {
                 new Thread(() -> {
                     try {
                         handleIncomingConnection(incomingSocket);
-                    } catch (ClassNotFoundException | IOException e) {
+                    } catch (ClassNotFoundException | IOException | NoSuchAlgorithmException e) {
                         e.printStackTrace();
                     } finally {
                         try {
@@ -57,8 +58,8 @@ public class FileTransferManager {
         }
     }
 
-    // Handles an incoming connection by receiving chunks and processing them
-    private void handleIncomingConnection(Socket incomingSocket) throws IOException, ClassNotFoundException {
+    // Handles an incoming connection
+    private void handleIncomingConnection(Socket incomingSocket) throws IOException, ClassNotFoundException, NoSuchAlgorithmException {
         try (ObjectInputStream objectInputStream = new ObjectInputStream(incomingSocket.getInputStream())) {
             InetAddress receiveIPAddress = incomingSocket.getInetAddress();
             PeerNetworkInterface localPeerNetworkInterface = NetworkUtils.subnetMatch(localPeer, receiveIPAddress);
@@ -66,13 +67,25 @@ public class FileTransferManager {
 
             while (true) {
                 try {
-                    FileChunkMessage chunkMessage = (FileChunkMessage) objectInputStream.readObject();
+                    Object object = objectInputStream.readObject();
+                    if (object instanceof FileChunkMessage) {
+                        FileChunkMessage chunkMessage = (FileChunkMessage) object;
+    
+                        if (chunkMessage.getReceiver().equals(localPeer)) {
+                            System.out.printf("[%s] %s.%d received from %s successfully.\n", localIPAddress, chunkMessage.getFileMetadata(), chunkMessage.getChunkIndex(), chunkMessage.getSender().getPeerNetworkInterfaces());
+                            fileManager.saveChunk(chunkMessage);
+                        } else {
+                            forwardMessage(chunkMessage);
+                        }
+                    } else if (object instanceof PeerFileMetadataRequestMessage) {
+                        PeerFileMetadataRequestMessage metadataRequestMessage = (PeerFileMetadataRequestMessage) object;
 
-                    if (chunkMessage.getReceiver().equals(localPeer)) {
-                        System.out.printf("[%s] %s.%d received from %s successfully.\n", localIPAddress, chunkMessage.getFileMetadata(), chunkMessage.getChunkIndex(), chunkMessage.getSender().getPeerNetworkInterfaces());
-                        fileManager.saveChunk(chunkMessage);
-                    } else {
-                        forwardChunk(chunkMessage);
+                        if (metadataRequestMessage.getReceiver().equals(localPeer)) { 
+                            System.out.printf("[%s] File request received from %s successfully.\n", localIPAddress, metadataRequestMessage.getSender().getPeerNetworkInterfaces());
+                            sendChunks(metadataRequestMessage.getSender(), metadataRequestMessage.getFileMetadata(), metadataRequestMessage.getChunkIndices());
+                        } else {
+                            forwardMessage(metadataRequestMessage);
+                        }
                     }
                 } catch (EOFException e) {
                     // End of file/stream
@@ -82,9 +95,9 @@ public class FileTransferManager {
         }
     }
 
-    // Forwards a chunk to the next peer
-    public void forwardChunk(FileChunkMessage chunkMessage) throws IOException {
-        List<PeerNetworkInterface> route = localPeer.getRouteToPeer(chunkMessage.getReceiver());
+    // Forwards a message to the next peer
+    private void forwardMessage(Message message) throws IOException {
+        List<PeerNetworkInterface> route = localPeer.getRouteToPeer(message.getReceiver());
         if (route == null) {
             return;
         }
@@ -98,9 +111,9 @@ public class FileTransferManager {
         try (Socket outputSocket = new Socket(targetIPAddress, LISTENING_PORT);
              ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputSocket.getOutputStream())) {
 
-            System.out.printf("[%s] Message of %s forwarded to %s successfully.\n", localIPAddress, chunkMessage.getSender().getPeerNetworkInterfaces(), chunkMessage.getReceiver().getPeerNetworkInterfaces());
+            System.out.printf("[%s] Message of %s forwarded to %s successfully.\n", localIPAddress, message.getSender().getPeerNetworkInterfaces(), message.getReceiver().getPeerNetworkInterfaces());
 
-            objectOutputStream.writeObject(chunkMessage);
+            objectOutputStream.writeObject(message);
             objectOutputStream.flush();
         }
     }
@@ -122,7 +135,7 @@ public class FileTransferManager {
              ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputSocket.getOutputStream())) {
 
             for (int chunkIndex : chunkIndices) {
-                byte[] chunkData = fileManager.getChunk(fileMetadata, chunkIndex);
+                byte[] chunkData = fileManager.loadChunk(fileMetadata, chunkIndex);
                 FileChunkMessage chunkMessage = new FileChunkMessage(localPeer, receiver, fileMetadata, chunkIndex, chunkData);
                 
                 System.out.printf("[%s] %s.%d sent to %s successfully.\n", localIPAddress, fileMetadata, chunkIndex, receiver.getPeerNetworkInterfaces());
@@ -130,6 +143,29 @@ public class FileTransferManager {
                 objectOutputStream.writeObject(chunkMessage);
                 objectOutputStream.flush();
             }
+        }
+    }
+
+    public void requestChunks(Peer receiver, PeerFileMetadata fileMetadata, Set<Integer> chunkIndices) throws IOException {
+        List<PeerNetworkInterface> route = localPeer.getRouteToPeer(receiver);
+        if (route == null) {
+            return;
+        }
+
+        PeerNetworkInterface targetPeerNetworkInterface = route.get(0);
+        InetAddress targetIPAddress = targetPeerNetworkInterface.getLocalIPAddress();
+
+        PeerNetworkInterface localPeerNetworkInterface = NetworkUtils.subnetMatch(localPeer, targetIPAddress);
+        InetAddress localIPAddress = localPeerNetworkInterface.getLocalIPAddress();
+
+        try (Socket outputSocket = new Socket(targetIPAddress, LISTENING_PORT);
+             ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputSocket.getOutputStream())) {
+
+            PeerFileMetadataRequestMessage metadataRequestMessage = new PeerFileMetadataRequestMessage(localPeer, receiver, fileMetadata, chunkIndices);
+            System.out.printf("[%s] File request sent to %s successfully.\n", localIPAddress, receiver.getPeerNetworkInterfaces());
+
+            objectOutputStream.writeObject(metadataRequestMessage);
+            objectOutputStream.flush();
         }
     }
 }
