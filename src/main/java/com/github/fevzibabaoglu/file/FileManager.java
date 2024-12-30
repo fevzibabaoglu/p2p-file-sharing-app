c44b70a005fde8982904361bc6dcc4a361187912
Fevzi Babaoğlu <fevzibabaoglu03@gmail.com>
2024-12-31 00:15:07 +0300

    feat: wait for arriving files before merge

--- src/main/java/com/github/fevzibabaoglu/file/FileManager.java
+++ src/main/java/com/github/fevzibabaoglu/file/FileManager.java
@@ -2,7 +2,9 @@ package com.github.fevzibabaoglu.file;
 
 import java.io.*;
 import java.nio.file.*;
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
+import java.util.Objects;
 import java.util.Random;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -12,19 +14,24 @@ import com.github.fevzibabaoglu.network.file_transfer.FileChunkMessage;
 
 public class FileManager {
 
-    private final String inputPath;
-    private final String outputPath;
+    private String sourcePath;
+    private String destinationPath;
     private final int chunkSize;
 
-    public FileManager(String inputPath, String outputPath, int chunkSize) {
-        this.inputPath = inputPath;
-        this.outputPath = outputPath;
+    public FileManager(String sourcePath, String destinationPath, int chunkSize) {
+        this.sourcePath = sourcePath;
+        this.destinationPath = destinationPath;
         this.chunkSize = chunkSize;
     }
 
+    public void setPaths(String inputPath, String outputPath) {
+        this.sourcePath = inputPath;
+        this.destinationPath = outputPath;
+    }
+
     // Reads a specific chunk from the file to send
-    public byte[] getChunk(PeerFileMetadata fileMetadata, int chunkIndex) throws IOException {
-        Path path = Paths.get(inputPath, fileMetadata.getFilename());
+    public byte[] loadChunk(PeerFileMetadata fileMetadata, int chunkIndex) throws IOException {
+        Path path = Paths.get(sourcePath, fileMetadata.getFilename());
 
         // Calculate the start position of the requested chunk
         long startPosition = (long) chunkIndex * chunkSize;
@@ -52,46 +59,58 @@ public class FileManager {
     }
 
     // Save an incoming chunk to disk
-    public void saveChunk(FileChunkMessage fileChunkMessage) throws IOException {
+    public PeerFileMetadata saveChunk(FileChunkMessage fileChunkMessage) throws IOException, NoSuchAlgorithmException {
         String chunkFilename = String.format("%s.%s", fileChunkMessage.getFileMetadata().getFilename(), fileChunkMessage.getChunkIndex());
-        Path path = Paths.get(outputPath, chunkFilename);
+        Path path = Paths.get(destinationPath, chunkFilename);
         try (BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(path.toString()))) {
             out.write(fileChunkMessage.getChunkData());
         }
+        return new PeerFileMetadata(path);
     }
 
     // Merge received chunks into a complete file
-    public void mergeChunks(List<String> chunkFilenames, String outputFilename) throws IOException {
-        try (BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(outputFilename))) {
-            for (String chunkPath : chunkFilenames) {
-                Path path = Paths.get(outputPath, chunkPath);
-    
-                // Ensure the chunk file exists
-                if (!Files.exists(path)) {
-                    throw new FileNotFoundException("Chunk not found: " + chunkPath);
-                }
+    public void mergeChunks(List<String> chunkFilenames, String outputFilename) throws IOException, InterruptedException {
+        // Wait until all chunk files arrive
+        while (!chunkFilenames.stream().map(chunkFilename -> Paths.get(destinationPath, chunkFilename)).allMatch(Files::exists)) {
+            Thread.sleep(500);
+        }
+
+        Path outputPath = Paths.get(destinationPath, outputFilename);
+        try (BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(outputPath.toString()))) {
+            for (String chunkFilename : chunkFilenames) {
+                Path path = Paths.get(destinationPath, chunkFilename);
     
                 // Read the entire chunk file and write it to the output file
                 byte[] chunkData = Files.readAllBytes(path);
                 out.write(chunkData);
+
+                // Delete the chunk file after writing
+                Files.delete(path);
             }
         }
     }
 
     // List files in a directory to be shared
     public Set<PeerFileMetadata> listSharedFiles() throws IOException {
-        try (Stream<Path> paths = Files.walk(Paths.get(inputPath))) {
+        try (Stream<Path> paths = Files.walk(Paths.get(sourcePath))) {
             return paths.filter(Files::isRegularFile)
-                        .map(path -> new PeerFileMetadata(path.toString()))
+                        .map(path -> {
+                            try {
+                                return new PeerFileMetadata(path);
+                            } catch (IOException | NoSuchAlgorithmException e) {
+                                return null;
+                            }
+                        })
+                        .filter(Objects::nonNull)
                         .collect(Collectors.toSet());
         }
     }
 
-    public void createRandomFile(String filename, int size) throws IOException {
-        Path filePath = Paths.get(inputPath, filename);
+    public void createRandomFile(String filename, int size, long seed) throws IOException {
+        Path filePath = Paths.get(sourcePath, filename);
         try (FileOutputStream fileOutputStream = new FileOutputStream(filePath.toString())) {
             byte[] buffer = new byte[1024];
-            Random random = new Random();
+            Random random = (seed == -1) ? new Random() : new Random(seed);
 
             for (int i = 0; i < size / buffer.length; i++) {
                 random.nextBytes(buffer);
